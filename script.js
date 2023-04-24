2b669a50d056c5bac79f1b4a0b63d25e54a89ff1
jbaldwin0 <jac0b67@ymail.com>
2023-04-24 14:38:30 -0500

    Set gameboard module to use the previous turn function

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 let turn = 0;
 
 const gameBoard =  (() => {
-    const choice = (input) => input;
+    const gameTurn = () => {
+        if (turn == 0) {
+            turn++;
+            return human.token;
+        }
+        else if (turn == 1) {
+            turn--;
+            return computer.token;
+        }
+    };
+    return {gameTurn};
 })();
 
 const player = (token) => {
     return {token};
-    /*
-    if (turn == 0) {
-        // X on square method
-        turn++;
-    }
-    else if (turn == 1) {
-        // 0 on square
-        turn--;
-    }
-    */
 };
 
 human = player("X");
 computer = player("O");
 
-function gameTurn () {
-    if (turn == 0) {
-        turn++;
-        return human.token;
-    }
-    else if (turn == 1) {
-        turn--;
-        return computer.token;
-    }
-}
-
 let gameTiles = document.querySelectorAll(".game-tile");
 gameTiles.forEach( function(div) {
     div.addEventListener("click", function() {
         console.log("A");
-        div.innerHTML = gameTurn();
+        div.innerHTML = gameBoard.gameTurn();
     });
 });
