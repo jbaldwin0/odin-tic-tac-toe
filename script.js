55f74814c44ba2196269ea04a4aba1e061788c69
jbaldwin0 <jac0b67@ymail.com>
2023-04-23 21:46:52 -0500

    Set test function to game tiles

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const gameBoard =  (() => {
-
+    const choice = () => 1+1; // filler
 })();
 
-const player = () => {
-    
+const player = (input) => {
+    let turn = 0;
+
+    /*
+    if (turn == 0) {
+        // X on square method
+        turn++;
+    }
+    else if (turn == 1) {
+        // 0 on square
+        turn--;
+    }
+    */
 };
+
+let gameTiles = document.querySelectorAll(".game-tile");
+gameTiles.forEach( function(div) {
+    div.addEventListener("click", function() {
+        console.log("A");
+    });
+});
